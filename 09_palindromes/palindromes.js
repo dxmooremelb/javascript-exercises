bd4e5a91c8f7579340afafc2ae397fb5074673aa
dxmooremelb <danielmoore.melb@gmail.com>
2023-10-15 23:35:10 +1100

    finished exer 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    const pdArray = string.toLowerCase().replace(/\W/g, '' ).split('');
+    const pdReversed = pdArray.map(index => index).reverse();
+
+    const string1 = pdArray.toString();
+    const string2 = pdReversed.toString();
+
+    return (string1 === string2);
+
 
 };
 
