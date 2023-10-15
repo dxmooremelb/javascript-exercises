70fbc8e0133d2adb5ef1b2f2a580b248d5fa5818
dxmooremelb <danielmoore.melb@gmail.com>
2023-10-15 23:07:06 +1100

    calc exercise done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  
+  let sum = 0;
+
+  array.forEach(i => sum += i);
+
+  return sum;
+
 };
 
-const multiply = function() {
+const multiply = function(array) {
+
+  return array.reduce((total, num) => total * num);
 
 };
 
-const power = function() {
+const power = function(n1, n2) {
 	
+  return n1 ** n2;
+
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
+  
 };
 
 // Do not edit below this line
