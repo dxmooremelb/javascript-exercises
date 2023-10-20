f8cde7716398c73ae2357620922191833261ef15
dxmooremelb <danielmoore.melb@gmail.com>
2023-10-20 18:50:50 +1100

    finished exer 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+
+
+        const oldest = people.sort(function(a, b) {
+
+            if (a.yearOfDeath === undefined) {
+                a.yearOfDeath = new Date().getFullYear();
+            } else if (b.yearOfDeath === undefined) {
+                b.yearOfDeath = new Date().getFullYear();
+            }
+
+            const lastPerson = a.yearOfDeath - a.yearOfBirth;
+            const nextPerson = b.yearOfDeath - b.yearOfBirth;
+
+            return lastPerson > nextPerson ? -1 : 1;
+
+        });
+
+        return oldest[0];
+
 
 };
 
